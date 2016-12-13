8ae2df7fd2e952ef3428fdb86a492f2695462d3a
umeshramya <umeshbilagi@gmail.com>
2016-12-13 19:00:59 +0530

    created archives for packages and reviewss

--- template-parts/content-reviews-archive.php
+++ template-parts/content-reviews-archive.php
@@ -10,11 +10,13 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		  <?php the_title( sprintf( '<h2 class="entry-title-hospital"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content-hospital">
 		<?php
+
+
 				$review_description = get_post_meta($post->ID);
 				if(!empty($review_description['reviewer_name'][0])){
 				$reviewer_name = $review_description['reviewer_name'][0];
